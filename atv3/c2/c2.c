d27684df9ed06fc5fcc4f4d6553c6b2e4b85132a
nicolasvondolinger <nicolasvondolinger@gmail.com>
2023-09-08 11:57:20 -0300

    c2 done

--- atv3/c2/c2.c
+++ atv3/c2/c2.c
@@ -7,7 +7,7 @@ int main(int argc, char** argv){
   for (i = 0; i < 9; i++)
     a[i] = i;
     
-  for (i = 0; i < 10; i++){
+  for (i = 0; i < 9; i++){
     printf("%d ", a[i]);
   }
   printf("\n");
