8840c4e7ae7fb99e20f2385a1ebc433aaa7ed5b4
Nicolas <nicolasvondolinger@gmail.com>
2023-11-27 17:39:37 -0300

    tp3 analisamem

--- tp3/AnalisaMemoria/modCode/include/matrix.hpp
+++
@@ -1,8 +0,0 @@
-class matrix{
-private:
-    
-public:
-    int begin;
-    int end;
-    long int m[2][2] = {{1, 0}, {0, 1}};
-};
--- tp3/src/main.cpp
+++ tp3/src/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
+
 #include "matrix.hpp"
+#include "../include/memlog.hpp"
 
 using namespace std;
 
@@ -20,6 +22,9 @@ matrix mult(matrix a, matrix b) {
 
             for (int k = 0; k < 2; k++) {
                 result.m[i][j] += a.m[i][k] * b.m[k][j];
+                LEMEMLOG((long int)(&(a.m[i][k])), sizeof(double), a.m[i][k]);
+                LEMEMLOG((long int)(&(b.m[k][j])), sizeof(double), b.m[k][j]);
+                ESCREVEMEMLOG((long int)(&(result.m[i][j])), sizeof(double), result.m[i][j]);
             }
             result.m[i][j] %= 100000000;  // Applying modulo to avoid overflow
         }
@@ -48,8 +53,10 @@ matrix query(int a, int b, int p, int l, int r) {
 
 // Recursive function to update a matrix in the segment tree
 matrix update(int i, matrix x, int p, int l, int r) {
-    if (i < l || r < i)
+    if (i < l || r < i){
+        
         return seg[p];
+    }
     if (l == r)
         return seg[p] = x;
     int m = (l + r) / 2;
@@ -70,6 +77,16 @@ int main() {
     int n, q;
     cin >> n >> q;
 
+    char lognome[100] = "/tmp/matrix.out";
+    int regmem = 1;
+
+    iniciaMemLog(lognome);
+
+    if(regmem) ativaMemLog();
+    else desativaMemLog();
+
+    defineFaseMemLog(0);
+
     for (int i = 0; i < q; i++) {
         char op;
         cin >> op;
@@ -79,6 +96,7 @@ int main() {
 
         switch (op) {
         case 'u':
+            defineFaseMemLog(0);
             int p;
             cin >> p;
             cin >> novaM.m[0][0] >> novaM.m[0][1] >> novaM.m[1][0] >> novaM.m[1][1];
@@ -86,6 +104,7 @@ int main() {
             update(p, novaM, 1, 0, n - 1);
             break;
         case 'q':
+            defineFaseMemLog(0);
             int b, d;
             cin >> b >> d;
             cin >> point.m[0][0] >> point.m[1][0];
